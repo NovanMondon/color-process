c18c87aaacb67736f01f361ad3fd96c37a978c91
novanmondon <newworld01.bright@gmail.com>
2024-12-10 05:48:51 +0900

    refactor

--- src/ColorPicker.tsx
+++ src/ColorPicker.tsx
@@ -11,32 +11,23 @@ import { pixelUtil, PixelData } from "./pixelUtil"
 export function ColorPicker({ state, setState }: { state: AppState, setState: SetAppState }) {
     const [tPointedColor, setPointedColor] = useState([0, 0, 0])
     const [tImageData, setImageData] = useState<PixelData | null>(null)
+    const [tImageFile, setImageFile] = useState<File | null>(null)
 
+    // file => image
+    useEffect(() => {
+        if (!tImageFile) return
+        const tImage = new Image()
+        tImage.src = URL.createObjectURL(tImageFile)
+        tImage.onload = () => { setState(state.update({ image: tImage })) }
+    }, [tImageFile])
+
+    // image => imageData
     useEffect(() => {
         if (!state.image) return
         const tImageData = pixelUtil.Element2ImageData(state.image)
         if (!tImageData) return
         setImageData(tImageData)
     }, [state.image])
-
-    // ファイルを選択して画像を読み込む
-    const handleFileChange = (aEvent: React.ChangeEvent<HTMLInputElement>) => {
-        const tFile = aEvent.target.files?.[0]
-        if (!tFile) return
-        const tImage = new Image()
-        tImage.src = URL.createObjectURL(tFile)
-        tImage.onload = () => { setState(new AppState({ image: tImage })) }
-    }
-
-    // ファイルをドロップして画像を読み込む
-    const handleDrop = (aEvent: React.DragEvent<HTMLDivElement>) => {
-        aEvent.preventDefault()
-        const tFile = aEvent.dataTransfer.files?.[0]
-        if (!tFile) return
-        const tImage = new Image()
-        tImage.src = URL.createObjectURL(tFile)
-        tImage.onload = () => { setState(new AppState({ image: tImage })) }
-    }
 
     // カラーピッカー
     const pickColor = (_: React.MouseEvent<HTMLDivElement>) => {
@@ -60,9 +51,9 @@ export function ColorPicker({ state, setState }: { state: AppState, setState: Se
         <>
             <div css={css(HorizontalFlex)}>
                 <div css={{ height: 750, width: 750 }}>
-                    <input type="file" onChange={handleFileChange} />
+                    <input type="file" onChange={(aEvent) => setImageFile(aEvent.target.files?.[0] || null)} />
                     <div css={css({ border: '1px solid black', overflow: 'scroll', height: 720, width: 720 })}
-                        onDrop={handleDrop}
+                        onDrop={(aEvent) => { aEvent.preventDefault(); setImageFile(aEvent.dataTransfer.files?.[0] || null) }}
                         onDragOver={(aEvent) => aEvent.preventDefault()}
                     >
                         {state.image &&
