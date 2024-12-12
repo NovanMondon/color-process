8e17a8a7b43934355f412fce90b832a4b513491a
novanmondon <newworld01.bright@gmail.com>
2024-12-12 09:24:41 +0900

    add color slider component

---
+++ src/ColorSliderComponent.tsx
@@ -0,0 +1,152 @@
+/** @jsxImportSource @emotion/react */
+
+import { AppState, SetAppState } from "./appState"
+import { useEffect, useState } from "react"
+import { PixelData, pixelUtil } from "./pixelUtil"
+import { Slider1D } from "./Slider1D"
+import { ImageCanvas } from "./ImageCanvas"
+import { Slider2D } from "./Slider2D"
+import { HorizontalFlex } from "./Styles"
+import { css } from "@emotion/react"
+
+type ColorSliderComponentProps = {
+    state: AppState,
+    setState: SetAppState,
+    flagRealtime: boolean,
+    valueMax: number[],
+    valueMin: number[],
+    valueStep: number[],
+    valueInit: { [index: number]: number }
+    value2Color: (value: { [index: number]: number }) => number[]
+    color2Value: (color: number[]) => { [index: number]: number }
+    normalizeValue: (value: { [index: number]: number }) => [boolean, { [index: number]: number }]
+}
+
+export const ColorSliderComponent = (
+    { state, setState, flagRealtime, valueInit, valueMax, valueMin, valueStep, value2Color, color2Value, normalizeValue }: ColorSliderComponentProps
+) => {
+    const [tValue, setValue] = useState<{ [index: number]: number }>(valueInit)
+
+    const [t2DTarget, set2DTarget] = useState<[number, number]>([-1, -1])
+    const [tValue2D, setValue2D] = useState<[number, number]>([0, 0])
+
+    const [tSliderPixels, setSliderPixels] = useState<PixelData[]>([])
+    const [t2DSliderPixels, set2DSliderPixels] = useState<PixelData[]>([])
+
+    // 初期化処理
+    useEffect(() => {
+        if (!state.isColorSanitized()) {
+            console.log("Color not sanitized")
+        } else {
+            setValue(color2Value(state.color))
+        }
+    }, [])
+
+    // スライダー更新処理
+    useEffect(() => {
+        // 値のチェック
+        const [flag, tNewValue] = normalizeValue([tValue[0], tValue[1], tValue[2]])
+        if (flag) {
+            setValue({ 0: tNewValue[0], 1: tNewValue[1], 2: tNewValue[2] })
+            return
+        }
+
+        // 色の更新
+        setState(state.update({ color: value2Color(tValue) }))
+    }, [tValue])
+
+    // 2Dスライダー更新処理
+    useEffect(() => {
+        // 値のチェック
+        const tUpdatedValue = { ...tValue, [t2DTarget[0]]: tValue2D[0], [t2DTarget[1]]: tValue2D[1] }
+        const [flag, tNewValue] = normalizeValue([tUpdatedValue[0], tUpdatedValue[1], tUpdatedValue[2]])
+        if (flag) {
+            setValue2D([tNewValue[t2DTarget[0]], tNewValue[t2DTarget[1]]])
+            return
+        }
+
+        // 値の更新
+        setValue(tUpdatedValue)
+    }, [tValue2D])
+
+    // 2Dスライダーのターゲットが変更された場合の処理
+    useEffect(() => {
+        if (t2DTarget[0] < 0 || t2DTarget[1] < 0) {
+            return
+        }
+        setValue2D([tValue[t2DTarget[0]], tValue[t2DTarget[1]]])
+    }, [t2DTarget])
+
+    // スライダー画像の更新
+    useEffect(() => {
+        // 1Dスライダー
+        if (flagRealtime) {
+            let tPixels = []
+            for (let i = 0; i < 3; i++) {
+                tPixels.push(pixelUtil.GeneratePixelFromFunc(300, 1, (x, _) => {
+                    const tTempValue = { ...tValue, [i]: x * valueMax[i] }
+                    return value2Color(tTempValue)
+                })
+                )
+            }
+            setSliderPixels(tPixels)
+        } else {
+            let tPixels = []
+            for (let i = 0; i < 3; i++) {
+                tPixels.push(pixelUtil.GeneratePixelFromFunc(300, 1, (x, _) => {
+                    const tTempValue = { ...valueInit, [i]: x * valueMax[i] }
+                    return value2Color(tTempValue)
+                })
+                )
+            }
+            setSliderPixels(tPixels)
+        }
+
+        // 2Dスライダー
+        if (flagRealtime) {
+            const t2DSliderPixel: PixelData = pixelUtil.GeneratePixelFromFunc(300, 300, (x, y) => {
+                const tTempValue = { ...tValue, [t2DTarget[0]]: x * valueMax[t2DTarget[0]], [t2DTarget[1]]: y * valueMax[t2DTarget[1]] }
+                return value2Color(tTempValue)
+            })
+            set2DSliderPixels([t2DSliderPixel])
+        } else {
+            const t2DSliderPixel: PixelData = pixelUtil.GeneratePixelFromFunc(300, 300, (x, y) => {
+                const tTempValue = { ...valueInit, [t2DTarget[0]]: x * valueMax[t2DTarget[0]], [t2DTarget[1]]: y * valueMax[t2DTarget[1]] }
+                return value2Color(tTempValue)
+            })
+            set2DSliderPixels([t2DSliderPixel])
+        }
+    }, [tValue, flagRealtime, t2DTarget])
+
+    const is2DSliderEnabled = () => { return t2DTarget[0] >= 0 && t2DTarget[1] >= 0 && t2DTarget[0] !== t2DTarget[1] }
+
+    return (
+        <>
+            {[0, 1, 2].map((index) => !(is2DSliderEnabled() && (index === t2DTarget[0] || index === t2DTarget[1])) && (
+                <div key={index} css={css(HorizontalFlex)} >
+                    <Slider1D value={tValue[index] ?? 0} min={0} max={255} step={1} setValue={(v) => setValue({ ...tValue, [index]: v })}>
+                        <ImageCanvas pixelData={tSliderPixels[index]} />
+                    </Slider1D>
+                    <label>
+                        <input type="radio" name="2DTarget0" value={index} onChange={() => set2DTarget([index, t2DTarget[1]])} />
+                        2D x
+                    </label>
+                    <label>
+                        <input type="radio" name="2DTarget1" value={index} onChange={() => set2DTarget([t2DTarget[0], index])} />
+                        2D y
+                    </label>
+                </div>
+            ))}
+            {is2DSliderEnabled() && (
+                <Slider2D
+                    value={tValue2D}
+                    min={[valueMin[t2DTarget[0]], valueMin[t2DTarget[1]]]}
+                    max={[valueMax[t2DTarget[0]], valueMax[t2DTarget[1]]]}
+                    step={[valueStep[t2DTarget[0]], valueStep[t2DTarget[1]]]}
+                    setValue={(v) => setValue2D(v)}>
+                    <ImageCanvas pixelData={t2DSliderPixels[0]} />
+                </Slider2D>
+            )}
+        </>
+    )
+}
--- src/ColorSliderRGB.tsx
+++ src/ColorSliderRGB.tsx
@@ -1,147 +1,12 @@
 /** @jsxImportSource @emotion/react */
 
 import { AppState, SetAppState } from "./appState"
-import { useEffect, useState } from "react"
-import { PixelData, pixelUtil } from "./pixelUtil"
-import { Slider1D } from "./Slider1D"
-import { ImageCanvas } from "./ImageCanvas"
-import { Slider2D } from "./Slider2D"
-import { HorizontalFlex } from "./Styles"
-import { css } from "@emotion/react"
+import { ColorSliderComponent } from "./ColorSliderComponent"
 
 const RGB_MAX = [255, 255, 255]
 const RGB_MIN = [0, 0, 0]
 const RGB_STEP = [1, 1, 1]
 const RGB_INIT = { 0: 0, 1: 0, 2: 0 }
-
-export const ColorSliderRGB = ({ state, setState, flagRealtime }: { state: AppState, setState: SetAppState, flagRealtime: boolean }) => {
-    const [tRGB, setRGB] = useState<{ [index: number]: number }>(RGB_INIT)
-
-    const [t2DTarget, set2DTarget] = useState<[number, number]>([-1, -1])
-    const [tValue2D, setValue2D] = useState<[number, number]>([0, 0])
-
-    const [tSliderPixels, setSliderPixels] = useState<PixelData[]>([])
-    const [t2DSliderPixels, set2DSliderPixels] = useState<PixelData[]>([])
-
-    // 初期化処理
-    useEffect(() => {
-        console.log("ColorSliderRGB")
-        if (!state.isColorSanitized()) {
-            console.log("Color not sanitized")
-        } else {
-            setRGB({ 0: state.color[0], 1: state.color[1], 2: state.color[2] })
-        }
-    }, [])
-
-    // スライダー更新処理
-    useEffect(() => {
-        // console.log("RGB: ", tRGB)
-        // 値のチェック
-        const [flag, tNewRGB] = normalizeRGB([tRGB[0], tRGB[1], tRGB[2]])
-        if (flag) {
-            setRGB({ 0: tNewRGB[0], 1: tNewRGB[1], 2: tNewRGB[2] })
-            return
-        }
-
-        // 色の更新
-        setState(state.update({ color: [tRGB[0], tRGB[1], tRGB[2]] }))
-    }, [tRGB])
-
-    // 2Dスライダー更新処理
-    useEffect(() => {
-        // 値のチェック
-        const tRGB_ = { ...tRGB, [t2DTarget[0]]: tValue2D[0], [t2DTarget[1]]: tValue2D[1] }
-        const [flag, tNewRGB] = normalizeRGB([tRGB_[0], tRGB_[1], tRGB_[2]])
-        if (flag) {
-            setValue2D([tNewRGB[t2DTarget[0]], tNewRGB[t2DTarget[1]]])
-            return
-        }
-
-        // 値の更新
-        setRGB(tRGB_)
-    }, [tValue2D])
-
-    // 2Dスライダーのターゲットが変更された場合の処理
-    useEffect(() => {
-        if (t2DTarget[0] < 0 || t2DTarget[1] < 0) {
-            return
-        }
-        setValue2D([tRGB[t2DTarget[0]], tRGB[t2DTarget[1]]])
-    }, [t2DTarget])
-
-    // スライダー画像の更新
-    useEffect(() => {
-        // 1Dスライダー
-        if (flagRealtime) {
-            let tPixels = []
-            for (let i = 0; i < 3; i++) {
-                tPixels.push(pixelUtil.GeneratePixelFromFunc(300, 1, (x, _) => {
-                    const tRGB_ = { ...tRGB, [i]: x * RGB_MAX[i] }
-                    return [tRGB_[0], tRGB_[1], tRGB_[2]]
-                })
-                )
-            }
-            setSliderPixels(tPixels)
-        } else {
-            let tPixels = []
-            for (let i = 0; i < 3; i++) {
-                tPixels.push(pixelUtil.GeneratePixelFromFunc(300, 1, (x, _) => {
-                    const tRGB_ = { ...RGB_INIT, [i]: x * RGB_MAX[i] }
-                    return [tRGB_[0], tRGB_[1], tRGB_[2]]
-                })
-                )
-            }
-            setSliderPixels(tPixels)
-        }
-
-        // 2Dスライダー
-        if (flagRealtime) {
-            const t2DSliderPixel: PixelData = pixelUtil.GeneratePixelFromFunc(300, 300, (x, y) => {
-                const tRGB_ = { ...tRGB, [t2DTarget[0]]: x * RGB_MAX[t2DTarget[0]], [t2DTarget[1]]: y * RGB_MAX[t2DTarget[1]] }
-                return [tRGB_[0], tRGB_[1], tRGB_[2]]
-            })
-            set2DSliderPixels([t2DSliderPixel])
-        } else {
-            const t2DSliderPixel: PixelData = pixelUtil.GeneratePixelFromFunc(300, 300, (x, y) => {
-                const tRGB_ = { ...RGB_INIT, [t2DTarget[0]]: x * RGB_MAX[t2DTarget[0]], [t2DTarget[1]]: y * RGB_MAX[t2DTarget[1]] }
-                return [tRGB_[0], tRGB_[1], tRGB_[2]]
-            })
-            set2DSliderPixels([t2DSliderPixel])
-        }
-    }, [tRGB, flagRealtime, t2DTarget])
-
-    const is2DSliderEnabled = () => { return t2DTarget[0] >= 0 && t2DTarget[1] >= 0 && t2DTarget[0] !== t2DTarget[1] }
-
-    return (
-        <>
-            {[0, 1, 2].map((index) => !(is2DSliderEnabled() && (index === t2DTarget[0] || index === t2DTarget[1])) && (
-                <div key={index} css={css(HorizontalFlex)} >
-                    <Slider1D value={tRGB[index] ?? 0} min={0} max={255} step={1} setValue={(v) => setRGB({ ...tRGB, [index]: v })}>
-                        <ImageCanvas pixelData={tSliderPixels[index]} />
-                    </Slider1D>
-                    <label>
-                        <input type="radio" name="2DTarget0" value={index} onChange={() => set2DTarget([index, t2DTarget[1]])} />
-                        2D x
-                    </label>
-                    <label>
-                        <input type="radio" name="2DTarget1" value={index} onChange={() => set2DTarget([t2DTarget[0], index])} />
-                        2D y
-                    </label>
-                </div>
-            ))}
-            {is2DSliderEnabled() && (
-                <Slider2D
-                    value={tValue2D}
-                    min={[RGB_MIN[t2DTarget[0]], RGB_MIN[t2DTarget[1]]]}
-                    max={[RGB_MAX[t2DTarget[0]], RGB_MAX[t2DTarget[1]]]}
-                    step={[RGB_STEP[t2DTarget[0]], RGB_STEP[t2DTarget[1]]]}
-                    setValue={(v) => setValue2D(v)}>
-                    <ImageCanvas pixelData={t2DSliderPixels[0]} />
-                </Slider2D>
-            )}
-        </>
-    )
-}
 
 function normalizeRGB(rgb: number[]): [boolean, number[]] {
     if (rgb.length !== 3) {
@@ -162,4 +27,23 @@ function normalizeRGB(rgb: number[]): [boolean, number[]] {
         }
     }
     return [tFlag, tRGB]
+}
+
+export const ColorSliderRGB = ({ state, setState, flagRealtime }: { state: AppState, setState: SetAppState, flagRealtime: boolean }) => {
+    return (
+        <>
+            <ColorSliderComponent
+                state={state}
+                setState={setState}
+                flagRealtime={flagRealtime}
+                valueMax={RGB_MAX}
+                valueMin={RGB_MIN}
+                valueStep={RGB_STEP}
+                valueInit={RGB_INIT}
+                value2Color={(value) => [value[0], value[1], value[2]]}
+                color2Value={(color) => ({ 0: color[0], 1: color[1], 2: color[2] })}
+                normalizeValue={(value) => normalizeRGB([value[0], value[1], value[2]])}
+            />
+        </>
+    )
 }
